7561694e51e1e07023c36c1728774eaca39de919
GangBenger0001 <2675705747@qq.com>
2026-03-21 19:06:53 +0800

    Add function prototype for max before main
    Added a function prototype for max before main to avoid compilation errors.

--- 函数运用.cpp
+++ 函数运用.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int max (int arr[],int b);
+//C++读代码是按顺序从上往下读，所以如果要将函数定义在main函数之后，则必须在main函数前先提及这个函数，不然编译时会报错，提示此函数不存在。
 int main() {
     int n;
     cin >> n;
